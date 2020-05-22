578905726a1cea88b6fedfe3cb83747073a3b272
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-05-22 16:04:57 +0530

    Added state in AddSubscriber and App components

--- src/App.js
+++ src/App.js
@@ -1,68 +1,44 @@
 import React, { Component } from 'react';
-import Header from './Header';
+import Header from './Header.js';
 import './App.css';
 
-/*class App extends Component {
-    render() {
-
-        return ( <
-            div >
-            <
-            Header / >
-            <
-            div className = 'body' >
-            <
-            button className = "addbtn" > Add < /button> <
-            div className = 'content' >
-            <
-            span className = "inputs" > Name < /span> <
-            span className = "inputs" > Phone < /span> < /
-            div > <
-            /div> < /
-            div >
-        );
-    }
-}*/
 class App extends Component {
 
-    render() {
-        let subscribers = [{
-                id: 1,
-                name: 'Ayush',
-                phone: '1111111111'
-            },
-            {
-                id: 2,
-                name: 'Areeb',
-                phone: "2222222222"
-            }
-        ];
-        return (
-          <div>
-            <Header heading="Phone Directory" />
-            <div className="component-body-container">
-              <button className="custom-btn add-btn">Add</button>
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    }
+  }
+
+  render() {
     
-              <div className="grid-container heading-container">
-                <span className="grid-item name-heading">Name</span>
-                <span className="grid-item phone-heading">Phone</span>
-              </div>
-    
-              {
-                subscribers.map(sub => {
-                  return <div key={sub.id} className="grid-container">
-                    <span className="grid-item">{sub.name}</span>
-                    <span className="grid-item">{sub.phone}</span>
-                    <span className="grid-item action-btn-container">
-                      <button className="custom-btn delete-btn">Delete</button>
-                    </span>
-                  </div>
-                })
-              }
-            </div>
+    return (
+      <div>
+        <Header heading="Phone Directory" />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
           </div>
-        );
-        }
+
+          {
+            this.state.subscribersListToShow.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
