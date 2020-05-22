f81df3db15012bf6ab4189888eb34dbf82123ce2
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-05-22 15:15:39 +0530

    Added ‘AddSubscriber’ component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Header.css'
 
-const Header = function(props) {
+const Header = function(propertie) {
     return ( 
         <div className = 'header' >
-        {props.heading}
+            {propertie.heading}
         </div>
     )
 }
+
+/*import  React, {Component} from 'react';
+class Header extends Component {
+    render() {
+       return (
+          <div className = 'header'>
+             {this.props.heading}
+          </div>
+        )
+    }
+ }*/
+
 export default Header;
